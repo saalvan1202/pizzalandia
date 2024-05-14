5d2560e00265c09202175d4c3741ebd6776e9524
saalvan1202 <ashande75@gmail.com>
2024-05-14 01:20:24 -0500

    Inicio

--- src/Modulos/Ventas/Home.jsx
+++ src/Modulos/Ventas/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import Inicio from "./components/Inicio";
+import "./Home.scss";
 export default function Home() {
-  return "home";
+  return (
+    <div className="home">
+      <Inicio />
+    </div>
+  );
 }
